e72f756b1ef12fa228d23bbe7d1fcf36b154e9a3
sgyoo1 <sgyoo1@cookapps.com>
2024-11-19 21:42:08 +0900

    Revert "[fix]"
    This reverts commit 3b60337c7c614b632eb4c852b12a9ea469d30434.

--- Assets/_Project/Script/InGameManager.cs
+++ Assets/_Project/Script/InGameManager.cs
@@ -17,14 +17,12 @@ public enum RouletteState
 public class InGameManager : SingletonMonoBehaviour<InGameManager>
 {
     [SerializeField] private Button _spinButton;
+    [SerializeField] private GameObject _rewardPopup;
     [SerializeField] private TextMeshProUGUI _descText;
     [SerializeField] private TextMeshProUGUI _titleText;
     [SerializeField] private Image _selectedItemImage;
     [SerializeField] private Animator _rouletteAnimator;
     [SerializeField] private Animator _uiAnimator;
-    [SerializeField] private Animator _popupAnimator;
-
-    [SerializeField] private List<Sprite> _sprites;
 
     private RouletteState _currentState = RouletteState.Idle;
     private Item _selectedItem;
@@ -36,7 +34,7 @@ public class InGameManager : SingletonMonoBehaviour<InGameManager>
 
     public void OnClickResultButton()
     {
-        _popupAnimator.SetTrigger("PopupClose");
+        _rewardPopup.SetActive(false);
         UpdateState(RouletteState.Idle);
     }
 
@@ -89,9 +87,7 @@ public class InGameManager : SingletonMonoBehaviour<InGameManager>
                 {
                     if (onComplete)
                     {
-                        _popupAnimator.SetTrigger("PopupOpen");
-                        int findIndex = RouletteApiManager.Instance.CachedItems.FindIndex(l => l.name == _selectedItem.name);
-                        SetPopUp(findIndex);
+                        _rewardPopup.SetActive(true);
                     }
                 });
                 // 상태 변경 : OnClickResultButton()
@@ -105,13 +101,6 @@ public class InGameManager : SingletonMonoBehaviour<InGameManager>
         {
             UpdateState(RouletteState.Spinning);
         }
-    }
-
-    private void SetPopUp(int findIndex)
-    {
-        _titleText.text = "타이틀 텍스트";
-        _descText.text = "설명 텍스트";
-        // _selectedItemImage.sprite = _sprites[findIndex];
     }
 
     public Item GetRandomItemByWeight()
